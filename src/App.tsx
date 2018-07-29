5bf548043522a12a6dee675c8d8d5a80efd3732b
Brian Gebel <bgebel@gofundme.com>
2018-07-29 11:55:20 -0700

    Add less

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
 import * as React from 'react';
 import './App.css';
-
+import { Button } from 'semantic-ui-react';
 import logo from './logo.svg';
 
 class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    public render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.tsx</code> and save to reload.
+                    <Button>Press Me</Button>
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
