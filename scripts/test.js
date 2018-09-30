22907023468446325d5ee6f6040eed270c1bd80d
Brian Gebel <bgebel@gofundme.com>
2018-09-29 21:11:34 -0700

    Update jest

--- scripts/test.js
+++ scripts/test.js
@@ -1,30 +1,30 @@
-'use strict';
+/**
+ * Initialize env vars
+ */
+process.env.BABEL_ENV = "test";
+process.env.NODE_ENV = "test";
+process.env.PUBLIC_URL = "";
 
-// Do this as the first thing so that any code reading it knows the right env.
-process.env.BABEL_ENV = 'test';
-process.env.NODE_ENV = 'test';
-process.env.PUBLIC_URL = '';
-
+const jest = require("jest");
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
 // terminate the Node.js process with a non-zero exit code.
-process.on('unhandledRejection', err => {
-  throw err;
+process.on("unhandledRejection", err => {
+    throw err;
 });
 
 // Ensure environment variables are read.
-require('../config/env');
+require("../config/env");
 
-const jest = require('jest');
 let argv = process.argv.slice(2);
 
 // Watch unless on CI, in coverage mode, or explicitly running all tests
 if (
-  !process.env.CI &&
-  argv.indexOf('--coverage') === -1 &&
-  argv.indexOf('--watchAll') === -1
+    !process.env.CI &&
+    argv.indexOf("--coverage") === -1 &&
+    argv.indexOf("--watchAll") === -1
 ) {
-  argv.push('--watch');
+    argv.push("--watch");
 }
 
 
